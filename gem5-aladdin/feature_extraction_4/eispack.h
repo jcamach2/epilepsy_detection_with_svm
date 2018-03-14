bf666001856bdd175f243423ecff5917efd55187
markmatthewanderson <markmatthewanderson@gmail.com>
2018-03-14 21:07:38 +0000

    Added feature_extraction_4 (with pipelining)

---
+++ gem5-aladdin/feature_extraction_4/eispack.h
@@ -0,0 +1,9 @@
+double pythag ( double a, double b );
+double r8_epsilon ( );
+double r8_max ( double x, double y );
+double r8_min ( double x, double y );
+double r8_sign ( double x );
+int rs ( int n, double a[], double w[], bool matz, double z[] );
+int tqlrat ( int n, double w[], double fv2[] );
+void trbak1 ( int n, double a[], double e[], int m, double z[] );
+void tred1 ( int n, double a[], double w[], double fv1[], double fv2[] );
